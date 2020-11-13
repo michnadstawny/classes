7273f72529795a7289812dfede10d281f39a2dd5
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-13 18:45:34 +0100

    created BankAccount.cs, next tutorial step

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace classes
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace classes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Michal", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
         }
     }
 }
